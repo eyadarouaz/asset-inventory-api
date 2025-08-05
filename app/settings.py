2a691c5b7cd206fd9af5aa5b3bb281bec3047a6e
Eya <eyaadarouez@gmail.com>
2025-08-05 11:23:28 +0100

    Add CORS policy for client app

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_yasg",
     "app",
+    "corsheaders",
 ]
 
 AUTH_USER_MODEL = "app.User"
@@ -67,6 +68,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -144,3 +146,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    env("CLIENT_API_URL")
+]
