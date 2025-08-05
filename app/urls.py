13e25ad037b0345210114a6caaa9f0210b55ec31
Eya <eyaadarouez@gmail.com>
2025-08-05 11:24:05 +0100

    Add get own profile path

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from app.swagger import schema_view
-from app.views.auth_views import MyTokenObtainPairView, mfa_setup
+from app.views.auth_views import MyTokenObtainPairView, get_me, mfa_setup
 from app.views.viewsets import (DataCenterViewSet, DiskArrayViewSet,
                                 MaintenanceRecordViewSet,
                                 ServerDiskArrayMapViewSet, ServerViewSet,
@@ -25,6 +25,7 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("api/login/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/me/", get_me, name="get-me"),
     path("api/mfa/setup/", mfa_setup, name="mfa_setup"),
     path("api/", include(router.urls)),
 ]
